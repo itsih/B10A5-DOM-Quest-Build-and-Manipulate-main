1ce895c7c646a91e43612c558945e5f9fc6ab7ff
Ibrahim Hossain <ibrahimhossainakib@gmail.com>
2024-09-26 22:18:21 +0600

    transaction history section completed

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ document.getElementById("donate-btn-1")
     const balance = getTextFieldValueById('account-balance');
     const amount1 = getTextFieldValueById('amount-1');
 
+
+
     const donatedAmount1 = amount1 + donate1;
     const newBalance = balance - donate1;
     document.getElementById('account-balance').innerText = newBalance.toFixed(2);
     document.getElementById('amount-1').innerText = donatedAmount1.toFixed(2);
+
+    const div = document.createElement('div');
+    div.classList.add('rounded-xl','p-6','border','shadow-lg');
+    div.innerHTML = `
+    <h2 class="font-bold text-lg">${amount1} Tk is donated for Flood Relief in Noakhali,Bangladesh </h2>
+    <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>
+    `
+    document.getElementById('transaction-container').appendChild(div);
 
 });
 
@@ -24,6 +34,14 @@ document.getElementById("donate-btn-2")
     document.getElementById('account-balance').innerText = newBalance.toFixed(2);
     document.getElementById('amount-2').innerText = donatedAmount2.toFixed(2);
 
+    const div = document.createElement('div');
+    div.classList.add('rounded-xl','p-6','border','shadow-lg');
+    div.innerHTML = `
+    <h2 class="font-bold text-lg">${amount2} Tk is donated for Flood Relief in Feni,Bangladesh </h2>
+    <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>
+    `
+    document.getElementById('transaction-container').appendChild(div);
+
 });
 
 // card 3
@@ -37,5 +55,13 @@ document.getElementById("donate-btn-3")
     const newBalance = balance - donate3;
     document.getElementById('account-balance').innerText = newBalance.toFixed(2);
     document.getElementById('amount-3').innerText = donatedAmount3.toFixed(2);
+
+    const div = document.createElement('div');
+    div.classList.add('rounded-xl','p-6','border','shadow-lg');
+    div.innerHTML = `
+    <h2 class="font-bold text-lg">${amount3} Tk is donated for Flood Relief in Cumilla,Bangladesh </h2>
+    <p>Date: ${new Date().toLocaleDateString()} GMT +0600 (Bangladesh Standard Time)</p>
+    `
+    document.getElementById('transaction-container').appendChild(div);
 
 });
